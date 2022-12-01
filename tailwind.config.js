4546a416f40144a7a4d5115583b2cadd5a163b18
vattgern <sashawot060@gmail.com>
2022-12-01 15:37:53 +0400

    Сделал сайдбар

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      mainBg: "#E7E7E7",
+      asideBg: "#232323",
+      blocksBg: "#FFFFFF",
+      buttonsBg: "#D6D6D6",
+      textClr: "#000000",
+      lineAsClr: "#626262",
+      darkMainBg: "#232323",
+      darkBlocksBg: "#424242",
+      darkTextClr: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: []
+}
